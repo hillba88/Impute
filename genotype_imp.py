12526df12c2e565af76e7bea7f4f4a85b5f7bf0e
Ben Hill <Ben@Benjamin-Hills-MacBook-Pro.local>
2017-12-03 14:44:59 -0500

    fixed transition function

--- genotype_imp.py
+++ genotype_imp.py
@@ -14,32 +14,52 @@ parser.add_argument("-q", help="Minimum quality score")
 args = parser.parse_args()
 
 '''
-function takes two arguments: a list of state names, and an optional parameter
-specifying a probability distribution to use for each state. The default
-distribution specifies equal probabilities for all nucleotide bases
+This function creates states based on two input parameters: a
+list of state names, and an optional parameter specifying a probability
+distribution to use for each state. The default distribution specifies
+equal probabilities for all nucleotide bases. Returns list of states
 '''
 def states(name_states, dist=DiscreteDistribution({'A': 0.25, 'C': 0.25,
                                                    'G': 0.25, 'T': 0.25}))
     count = 0
+    state_list = []
     try:
         for x in range(len(name_states)):
             "s{0}".format(x) = State(dist, name=name_states[counter])
+            state_list.append("s{0}".format(x))
             count += 1
 
     except TypeError:
         print("{0} must be in list format".format(states))
 
-def transition_probs(num_states, ):
+    return state_list
+
+'''
+This function creates a HMM, adds states and state transitions. The function
+takes two arguments, a list of states and a transition probability matrix.
+'''
+def transition(states, transition):
     start_prob = float(100/states)
     hmm = HiddenMarkovModel
 
-    for x in range(states):
-        hmm.add_states(x)
-        hmm.add_transition(hmm.start, x, start_prob)
+    for item in states:
+        hmm.add_states(item)
+        hmm.add_transition(hmm.start, item, start_prob)
 
-    for x in range(states):
+    for x in range(len(states)):
+        hmm.add_transition(states[x], states[x], transition[x,x])
+        for y in range(x+1, len(states)):
+            hmm.add_transition(states[x], states[y], transition[y,x])
+
+    hmm.bake()
 
 
+'''
+Transition and emission probabilities obtained from
+    Swarts et al. (2014) Novel Methods to Optimize Genotypic Imputation for
+    Low-Coverage, Next-Generation Sequence Data in Crop Plants. The Plant
+    Genome, doi:10.3835/plantgenome2014.05.0023
+''''
 transition = np.matrix([[0.9990, 0.00010, 0.00300, 0.00010, 0.0005],
                         [0.0002, 0.99900, 0.00005, 0.00005, 0.0002],
                         [0.0002, 0.00005, 0.99900, 0.00005, 0.0002],
@@ -51,9 +71,3 @@ emission = np.matrix([[0.998, 0.001, 0.001],
                       [0.400, 0.200, 0.400],
                       [0.200, 0.200, 0.600],
                       [0.001, 0.001, 0.998]])
-
-hmm.add_transition( s1, s1, 0.5 )
-hmm.add_transition( s1, s2, 0.5 )
-hmm.add_transition( s2, s1, 0.5 )
-hmm.add_transition( s2, s2, 0.5 )
-hmm.bake()
