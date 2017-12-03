926a1d87bbefa28aaaf9547d1121582da8805c41
Ben Hill <Ben@Benjamin-Hills-MacBook-Pro.local>
2017-12-03 13:34:07 -0500

    added transition_probs function

--- genotype_imp.py
+++ genotype_imp.py
@@ -16,27 +16,33 @@ args = parser.parse_args()
 d1 = DiscreteDistribution({'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25})
 d2 = DiscreteDistribution({'A': 0.10, 'C': 0.40, 'G': 0.40, 'T': 0.10})
 
-def states(dist, states)
+'''
+function takes two arguments: a list of state names, and an optional parameter
+specifying a probability distribution to use for each state. The default
+distribution specifies equal probabilities for all nucleotide bases
+'''
+def states(name_states, dist=DiscreteDistribution({'A': 0.25, 'C': 0.25,
+                                                   'G': 0.25, 'T': 0.25}))
     count = 0
-    if type(states) is not list: states = [ states ]
 
-    for x in range(states):
-        "s{0}".format(x) = State(dist, name=states[counter])
-        count += 1
+    try:
+        for x in range(len(name_states)):
+            "s{0}".format(x) = State(dist, name=name_states[counter])
+            count += 1
 
-s1 = State( d1, name='AA' )
-s2 = State( d1, name='3A:1B' )
-s3 = State( d1, name='1A:1B' )
-s4 = State( d1, name='1A:3B' )
-s5 = State( d1, name='BB' )
+    except TypeError:
+        print("{0} must be in list format".format(states))
 
-def transition_probs(states):
+def transition_probs(num_states, ):
     start_prob = float(100/states)
     hmm = HiddenMarkovModel
 
     for x in range(states):
         hmm.add_states(x)
         hmm.add_transition(hmm.start, x, start_prob)
+
+    for x in range(states):
+
 
 
 
